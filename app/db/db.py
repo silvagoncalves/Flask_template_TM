07728c9dc46da46a97f430364b3ba739135c00bc
silvagoncalves <106246881+silvagoncalves@users.noreply.github.com>
2023-10-29 22:04:35 +0100

    changement de auth et home

--- app/db/db.py
+++ app/db/db.py
@@ -7,6 +7,7 @@ def get_db():
     # Il agit comme une variable globale pour chaque requête
     # Dans notre cas, si l'objet g n'a pas d'attribut 'db', on lui ajoute une connexion à la base de données
     if 'db' not in g:
+        #g.db = sqlite3.connect(os.path.join(current_app.root_path, "db","app.db" ), detect_types=sqlite3.PARSE_DECLTYPES)
         g.db = sqlite3.connect(os.path.join(current_app.root_path, "db", current_app.config['DATABASE']), detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = sqlite3.Row
     return g.db
