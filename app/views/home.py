2db7bdea70ce06fc2f8988deb0ed96d941f5bebc
silvagoncalves <106246881+silvagoncalves@users.noreply.github.com>
2024-01-03 13:41:15 +0100

    Commencement de la page list_teacher

--- app/views/home.py
+++ app/views/home.py
@@ -19,27 +19,38 @@ def research_teacher():
     
     db = get_db()
     levels = db.execute("SELECT * FROM level").fetchall()
+    subjects = db.execute("SELECT * FROM subject").fetchall()
+    course_types = db.execute("SELECT * FROM course_type").fetchall()
     
-    return render_template('home/research.html', levels=levels)
+    return render_template('home/research.html', levels=levels, subjects=subjects, course_types=course_types)
 
 @home_bp.route('/list_teacher', methods=('GET', 'POST'))
 def list_teacher():
-
-
+    
     if request.method == 'POST':
         level = request.form['level']
-        course = request.form['course_type']
-        subject = request.form.getlist('subjects[]')
+        course_type = request.form['course_type']
+        subject = request.form['subject']
 
         db = get_db()
-        teachers = db.execute("SELECT users.* FROM users, teacher_level WHERE role_id = 1 and teacher_level.teacher_id = users.id and teacher_level.level_id = ?", (level,)).fetchall()
-
+        #teachers = db.execute("SELECT users.* FROM users, teacher_level WHERE role_id = 1 and teacher_level.teacher_id = users.id and teacher_level.level_id = ?", (level)).fetchall()
+        #teachers = db.execute("SELECT users. *FROM users, teacher_subject WHERE role_id = 1 and teacher_subject.teacher_id = users.id and teacher_subject.id = ?", (subject)).fetchall() 
+        teachers = db.execute("""
+            SELECT DISTINCT users.* FROM users, teacher_level, teacher_subject, teacher_course_type WHERE role_id = 1 and teacher_level.teacher_id = users.id and teacher_level.level_id = ? and teacher_subject.teacher_id = users.id and teacher_subject.subject_id = ?
+        """, (level, subject)).fetchall()
         print(level)
         print(subject)
-        print(course)
+        print(course_type)
+
 
     return render_template('home/list_teacher.html', teachers=teachers)
- 
+
+@home_bp.route('/teacher_account', methods=('GET', 'POST'))
+def teacher_account():
+    
+    levels_teacher = db.execute("SELECT * FROM level").fetchall()
+    
+    return render_template('home/list_teacher.html', levels_teacher=levels_teacher)
 
 @home_bp.route('/<path:text>', methods=['GET', 'POST'])
 def not_found_error(text):
