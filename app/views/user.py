f78bfb148f35b9463c77a23a3d2b1fa4456dfd6e
silvagoncalves <106246881+silvagoncalves@users.noreply.github.com>
2024-03-01 19:09:51 +0100

    Fonction follow

--- app/views/user.py
+++ app/views/user.py
@@ -52,8 +52,6 @@ def count_teacher():
     db = get_db()
 
     teacher_id = request.args.get('teacher_id')
-
-
     teacher = db.execute("SELECT * FROM users WHERE id = ?", (teacher_id,)).fetchone()
 
     levels_teacher = db.execute("""
@@ -77,22 +75,32 @@ def count_teacher():
     tarif_teacher_row = db.execute("SELECT tarif FROM users WHERE id = ?", (teacher_id,)).fetchone()
     tarif_teacher = tarif_teacher_row[0] if tarif_teacher_row is not None else None
 
+    
     if request.method == 'POST':
         try:
-            grade = int(request.form['grade'])
-            if 1 <= grade <= 5:
-                db.execute("INSERT INTO evalue (grade, teacher_id) VALUES (?, ?)", (grade, teacher_id))
-                db.commit()
-            else:
-                return "La note doit être entre 1 et 5."
+            if 'follow' in request.form:
+                db.execute("INSERT INTO follow (student_id, teacher_id) VALUES (?, ?)", (g.user['id'], teacher_id))
+            elif 'unfollow' in request.form:
+                db.execute("DELETE FROM follow WHERE student_id = ? AND teacher_id = ?", (g.user['id'], teacher_id))
+            elif 'grade' in request.form:
+                grade = int(request.form['grade'])
+                if 1 <= grade <= 5:
+                    db.execute("INSERT INTO evalue (grade, teacher_id) VALUES (?, ?)", (grade, teacher_id))
+                else:
+                    return "La note doit être entre 1 et 5."
         except ValueError:
             return "La note doit être un nombre entier."
+        finally:
+            db.commit()
 
     all_grades = db.execute("SELECT grade FROM evalue WHERE teacher_id = ?", (teacher_id,)).fetchall()
     total = 0
     if all_grades:
         grades = [grade[0] for grade in all_grades]
-        total = sum(grades) / len(grades)
+        total = sum(grades) // len(grades)
+    
+    
+    follow = db.execute("SELECT * FROM follow WHERE student_id = ? AND teacher_id = ?", (g.user['id'], teacher_id)).fetchone()
 
     db.close()
-    return render_template('user/count_teacher.html', teacher=teacher, tarif_teacher=tarif_teacher, levels_teacher=levels_teacher, subjects_teacher=subjects_teacher, course_types=course_types, total=total)
+    return render_template('user/count_teacher.html', follow=follow, teacher=teacher, tarif_teacher=tarif_teacher, levels_teacher=levels_teacher, subjects_teacher=subjects_teacher, course_types=course_types, total=total)
